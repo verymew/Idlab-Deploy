2ca970e7742df2ae2f7bf651192f3fce9a7d900a
verymew <verymewfor@gmail.com>
2024-08-01 11:17:14 -0300

    Background effect adicionado

--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -1,23 +1,25 @@
 <section class="ftco-section bg-light">
     <div class="container">
-      <div class="row">
-
-        @foreach ($projects as $projects)
-        <div class="col-md-4 ftco-animate">
-            <div class="blog-entry">
-              <a href="blog-single.html" class="block-20" style="background-image: url('{{ asset('storage/' . $projects->image_path) }}');">
-              </a>
-              <div class="text d-flex py-3">
-                <div class="desc pl-6">
-                    <h3  style="overflow-wrap: break-word !important;" class="heading"><a href="#">{{ $projects->title }} </a></h3>
+        <div class="row">
+            @foreach ($projects as $project)
+                <div class="col-md-4 ftco-animate">
+                    <div class="blog-entry" data-aos-delay="200">
+                        <a href="{{ route('showproject', $project->id) }}" class="block-20"
+                            style="background-image: url('{{ asset('storage/' . $project->image_path) }}');">
+                        </a>
+                        <div class="text d-flex py-4">
+                            <div class="meta mb-3">
+                                <div><a>Publicado em:</a></div>
+                                <div><a>{{ \Carbon\Carbon::parse($project->created_at)->format('d/m/Y') }}</a>
+                                </div>
+                            </div>
+                            <div class="desc pl-3">
+                                <h3 class="heading"><a href="{{ route('showproject', $project->id) }}">{{ $project->title }}</a></h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
+            @endforeach
         </div>
-        @endforeach
-      </div>
     </div>
-  </section>
-
-
-
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,26 @@
 
 
 @section('content')
-
-<div class="hero-wrap js-fullheight">
-    <div class="overlay"></div>
-    <div class="container-fluid px-0">
-        <div id="barcar" class="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-start">
-            <img class="one-third js-fullheight align-self-end order-md-first img-fluid" id="logo-img" src="images/Logo (3).svg" alt="">
-          <div class="one-forth d-flex align-items-center ftco-animate js-fullheight">
-              <div class="text mt-5" style="">
-              <h1 class="mb-1">Ideas</h1><h1 style="color: rgb(110, 226, 100);">/Creative Lab</h1>
-              <p>Espaço dedicado ao desenvolvimento de software com foco na criatividade e na experiência do usuário
-              </p>
-             <!--<p><a href="#" class="btn btn-secondary px-4 py-3">Sobre</a></p>-->
+    <div class="hero-wrap js-fullheight">
+        <div class="overlay"></div>
+        <div class="container-fluid px-0">
+            <div id="barcar"
+                class="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-start">
+                <img class="one-third js-fullheight align-self-end order-md-first img-fluid" id="logo-img"
+                    src="images/Logo (3).svg" alt="">
+                <div class="one-forth d-flex align-items-center ftco-animate js-fullheight">
+                    <div class="text mt-5" style="">
+                        <h1 class="mb-1">Ideas</h1>
+                        <h1 style="color: rgb(110, 226, 100);">/Creative Lab</h1>
+                        <p>Espaço dedicado ao desenvolvimento de software com foco na criatividade e na experiência do
+                            usuário
+                        </p>
+                        <!--<p><a href="#" class="btn btn-secondary px-4 py-3">Sobre</a></p>-->
+                    </div>
+                </div>
             </div>
-          </div>
-          </div>
+        </div>
     </div>
-</div>
-
-    <x-activities :activities="$activities"></x-activities>
 
     <section class="ftco-section services-section bg-light">
         <div class="container">
@@ -31,17 +32,19 @@
             </div>
             <div class="row">
                 @foreach ($projects as $project)
-                <div style="word-break: break-word !important;" class="col-lg-4 col-md-6 d-flex align-self-stretch ftco-animate">
-                    <div class="media block-6 services d-flex flex-column">
-                        <div class="media-body pl-4 d-flex flex-column">
-                            <h3 class="heading">{{ $project->title }}</h3>
-                            <p>{{ $project->resume }}</p>
-                            <div class="mt-auto">
-                                <a href="{{ route('showproject', $project->id) }}" class="btn btn-secondary px-4 py-3">Ver mais</a>
+                    <div style="word-break: break-word !important;"
+                        class="col-lg-4 col-md-6 d-flex align-self-stretch ftco-animate">
+                        <div class="media block-6 services d-flex flex-column">
+                            <div class="media-body pl-4 d-flex flex-column">
+                                <h3 class="heading">{{ $project->title }}</h3>
+                                <p>{{ $project->resume }}</p>
+                                <div class="mt-auto">
+                                    <a href="{{ route('showproject', $project->id) }}" class="btn btn-primary px-4 py-3">Ver
+                                        mais</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
             </div>
         </div>

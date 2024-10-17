b75c28b54f5114cdcc3aec86aef8f3e6e940639a
verymew <verymewfor@gmail.com>
2024-10-17 17:26:33 -0300

    Fix: Subheader das postagens atualizado

--- resources/views/blogpost.blade.php
+++ resources/views/blogpost.blade.php
@@ -2,21 +2,27 @@
 
 @section('content')
 
-<x-subheader title="{{ $project->title }}"></x-subheader>
+<style>
+  .post{
+    display: flex;
+    justify-content: center;
+  }
+  .action{
+    width: 65%;
+  }
+</style>
+
+<x-subheaderpost></x-subheaderpost>
 
 
-<section class="ftco-section ftco-degree-bg">
-    <div class="container">
-      <div class="row">
-        <div  class="col-lg-8 ftco-animate" style="overflow-wrap: break-word !important;">
+<section class="post">
+    <div class="action">
           <p>
             <img src="{{ asset('storage/' . $project->image_path) }}" alt="" class="img-fluid">
           </p>
           <h2 class="mb-3">{{ $project->title }}</h2>
           <p>{{ $project->content }}
           </p>
-        </div>
-     </div>
     </div>
 </section>
 
---
+++ resources/views/components/subheaderpost.blade.php
@@ -0,0 +1,10 @@
+<style>
+    .subheader{
+        height: 200px;
+        background-color: blue;
+    }
+</style> 
+
+<div class="subheader">
+    <p>teste</p>
+</div>
